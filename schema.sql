cd8e031b7c1611942908368b62e8d7f863a1d506
DanielRaus <79841354+DanielRaus@users.noreply.github.com>
2024-12-12 14:54:29 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,38 @@
+CREATE TABLE Reservation (
+  id serial primary key
+);
+
+CREATE TABLE Floor (
+  id serial primary key,
+  number int not null
+);
+
+CREATE TABLE Room (
+  id serial primary key
+);
+
+CREATE TABLE Room_Type (
+  id serial primary key
+);
+
+CREATE TABLE Customer (
+  id serial primary key
+);
+
+CREATE TABLE Employee (
+  id serial primary key
+);
+
+CREATE TABLE Employee_Type (
+  id serial primary key
+);
+
+
+
+
+
+
+
+
+
 
