c6646836cafcdb144df29ddec1d42db13b013c3c
DanielRaus <79841354+DanielRaus@users.noreply.github.com>
2024-12-12 15:32:14 +0100

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,15 @@
 CREATE TABLE Reservation (
-  id serial primary key
+  id serial primary key,
+  entry_date date default current_date not null,
+  out_date date not null,
+  animals boolean,
+  price decimal(10,2),
+  is_arrived boolean,
+  number_people int,
+  fk_userId int,
+  fk_roomId int,
+  FOREIGN KEY (fk_userId) REFERENCES User (id),
+  FOREIGN KEY (fk_roomId) REFERENCES Room (id)
 );
 
 CREATE TABLE Floor (
@@ -26,7 +36,6 @@ CREATE TABLE Employee (
 CREATE TABLE Employee_Type (
   id serial primary key
 );
-
 
 
 
