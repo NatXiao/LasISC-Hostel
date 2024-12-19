bb9335fefc9db748812e0343c91ee6b5b175245e
NatXiao <natasha.xiao@outlook.com>
2024-12-19 15:15:26 +0100

    queries

--- queries.sql
+++ queries.sql
@@ -1,15 +1,16 @@
 -- Reservation request
 
 -- Get all available rooms at specific dates for specific number of people
-SELECT count(re.fk_roomId) FROM Reservation AS re
-JOIN Room AS r ON r.id = re.fk_roomId
-WHERE r.fk_room_typeId == 1
-GROUP BY 
-HAVING CURDATE() > out
+SELECT r.id AS room_id, r.number AS room_number, rt.name AS room_type, rt.unit_price AS price
+FROM Room r
+JOIN Room_Type rt ON r.fk_room_typeId = rt.id
+LEFT JOIN Reservation res ON r.id = res.fk_roomId 
+    AND NOT (res.out_date <= '2024-12-20' OR res.entry_date >= DATE_ADD('2024-12-20', INTERVAL 5 DAY))
+WHERE res.id IS NULL;
 -- still in work
 
--- get the price for the reservation of one specific people for the check out
-SELECT (rt.unit_price * (re.entry_date - re.out_date)) AS "Price" FROM Reservation as re
+-- get the price for the reservation of one specific reservation for the check out
+SELECT (re.number_people * rt.unit_price * (re.entry_date - re.out_date)) AS "Price" FROM Reservation as re
 JOIN Room AS r ON r.id = re.fk_roomId
 JOIN Room_Type AS rt ON rt.id = r.id
 JOIN People AS p ON p.id = re.fk_peopleId
@@ -36,8 +37,16 @@ JOIN Country ON Country.id = City.fk_countryId
 GROUP BY Country.name
 -- normally works
 
--- Percentage of no show
+-- no show
 SELECT id FROM Reservation as re
 WHERE re.has_arrived == False and CURDATE() < re.entry_date
--- still in work
+-- work, but still in work
+
+-- Percentage of no show
+SELECT (no_shows / total_reservations) * 100 AS no_show_percentage
+FROM (
+    SELECT COUNT(*) AS total_reservations, SUM(NOT has_arrived) AS no_shows
+    FROM Reservation
+    WHERE entry_date <= CURDATE()
+)
 
