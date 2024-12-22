b6f074ad68d053c6e991845e200f578e4851f348
fylis <filip.siliwoniuk@gmail.com>
2024-12-22 22:04:29 +0100

    Updated queries to work as expected

--- queries.sql
+++ queries.sql
@@ -1,52 +1,53 @@
--- Reservation request
+------------------------------------------  Reservation request
 
--- Get all available rooms at specific dates for specific number of people
-SELECT r.id AS room_id, r.number AS room_number, rt.name AS room_type, rt.unit_price AS price
-FROM Room r
-JOIN Room_Type rt ON r.fk_room_typeId = rt.id
-LEFT JOIN Reservation res ON r.id = res.fk_roomId 
-    AND NOT (res.out_date <= '2024-12-20' OR res.entry_date >= DATE_ADD('2024-12-20', INTERVAL 5 DAY))
-WHERE res.id IS NULL;
--- still in work
+--  Get all available rooms at specific dates for specific number of people
+SELECT r.id AS room_id, r.number AS room_number, rt.name AS room_type, r.max_people, rt.unit_price*5 AS total_price_5_nights
+FROM Room AS r
+JOIN Room_Type AS rt ON r.fk_room_typeId = rt.id
+WHERE r.id NOT IN (
+    SELECT res.fk_roomId
+    FROM Reservation AS res
+    WHERE NOT (res.out_date <= '2024-12-20' OR res.entry_date >= DATE_ADD('2024-12-20', INTERVAL '5 days')))
+AND r.max_people >= 2;
 
--- get the price for the reservation of one specific reservation for the check out
-SELECT (re.number_people * rt.unit_price * (re.entry_date - re.out_date)) AS "Price" FROM Reservation as re
-JOIN Room AS r ON r.id = re.fk_roomId
+--  Get the price for the reservation of one specific reservation for the check out
+SELECT 'CHF ' || (res.number_people * rt.unit_price * (res.out_date - res.entry_date)) || '.-' AS "Price"
+FROM Reservation AS res
+JOIN Room AS r ON r.id = res.fk_roomId
 JOIN Room_Type AS rt ON rt.id = r.id
-JOIN People AS p ON p.id = re.fk_peopleId
-WHERE p.lastname = 'Landry' AND p.firstname = 'Natasha'
--- normally works
+JOIN People AS p ON p.id = res.fk_peopleId
+WHERE p.lastname = 'Baumann' AND p.firstname = 'Chloe'
 
---Verify a person reservation
-SELECT re.entry_date, re.out_date FROM Reservation as re
-JOIN People AS p ON p.id = re.fk_peopleId
-WHERE p.lastname = 'Landry' AND p.firstname = 'Natasha'
+--  Verify a person reservation
+SELECT r.number AS room, res.entry_date, res.out_date
+FROM Reservation AS res
+JOIN People AS p ON res.fk_peopleId = p.id
+JOIN Room AS r ON res.fk_roomId = r.id
+WHERE p.lastname = 'Schneider' AND p.firstname = 'Amelia'
 
---Statistic
+------------------------------------------  Statistic
 
---Where do the clients come from (city)
-SELECT City.name, count(p.id) FROM People as p
-JOIN City ON City.id = p.fk_cityId
-GROUP BY City.name
--- normally works
+--  Where do the clients come from (city)
+SELECT c.name, COUNT(p.id)
+FROM People AS p
+JOIN City AS c ON c.id = p.fk_cityId
+GROUP BY c.name
 
---Where do the clients come from (Country)
-SELECT Country.name, count(p.id) FROM People as p
+--  Where do the clients come from (Country)
+SELECT Country.name, COUNT(p.id)
+FROM People AS p
 JOIN City ON City.id = p.fk_cityId
 JOIN Country ON Country.id = City.fk_countryId
 GROUP BY Country.name
--- normally works
 
--- no show
-SELECT id FROM Reservation as re
-WHERE re.has_arrived == False and CURDATE() < re.entry_date
--- work, but still in work
+--  No show
+SELECT res.id
+FROM Reservation AS res
+WHERE res.has_arrived = False
 
--- Percentage of no show
-SELECT (no_shows / total_reservations) * 100 AS no_show_percentage
+--  Percentage of no-show
+SELECT (no_shows * 100.0/ total_reservations)::INTEGER || '%' AS no_show_percentage
 FROM (
-    SELECT COUNT(*) AS total_reservations, SUM(NOT has_arrived) AS no_shows
+    SELECT COUNT(*) AS total_reservations, SUM(CASE WHEN NOT has_arrived THEN 1 ELSE 0 END) AS no_shows
     FROM Reservation
-    WHERE entry_date <= CURDATE()
-)
-
+    WHERE entry_date <= CURRENT_DATE)
